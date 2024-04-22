ac4b84de6e9f63da8df14093231d82dfacda0f9d
small potato <38529172+fuheng1993@users.noreply.github.com>
2024-04-22 20:34:42 +0800

    Reapply "持续更新"
    This reverts commit b02fcf60a31377eea3bd85c02c6726a0b1518a1a.

--- App/HttpController/SystemRouter.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-$system_router =  [
-    ['name'=>'/my','path'=>'/index/index/index','method'=>['GET','POST']],
-    ['name'=>'/my1','path'=>'/index/index/index','method'=>['GET','POST']],
-    ['name'=>'/my2','path'=>'/index/index/index','method'=>['GET','POST']],
-];
